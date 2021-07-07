a3c99b1d9f237a95fc27336c3fe0652c441ae5e2
Sohail Ali Shaik <Sohail Ali Shaik@DESKTOP-LKL797R>
2021-07-08 00:32:14 +0530

    Basics

---
+++ Almamatter/src/ASCII_Value.java
@@ -0,0 +1,14 @@
+
+public class ASCII_Value {
+
+	public static void main(String[] args) {
+	
+			char ch = 's';
+			int ascii = ch;
+			int castAscii = (int) ch;
+			System.out.println("The ASCII value of " + ch + " is: " + ascii);
+			 System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+
+	}
+
+}
---
+++ Almamatter/src/Print_an_integer.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Print_an_integer {
+
+	public static void main(String[] args) {
+		Scanner reader= new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number=reader.nextInt();
+		System.out.println("you entered: "+number);
+
+	}
+
+}
+
